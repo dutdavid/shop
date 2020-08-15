82a3347142f2203f1f5e949326bdf52bef87758a
david dut <dutdavid7@gmail.com>
2020-08-15 05:10:40 +0300

    new

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Interview</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>Interview</h1>
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <div class="container">
